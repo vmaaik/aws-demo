1f1b6dfbac48e41a4f81947da058ca1d4d4d3ef2
Michal Gebarowski <vmaaik@gmail.com>
2018-08-02 12:04:51 +0100

    add rds script

---
+++ scripts/rds.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+yum install httpd php php-mysql -y  
+yum update -y  
+chkconfig httpd on  
+service httpd start  
+echo "<?php phpinfo();?>" > /var/www/html/index.php
+cd /var/www/html  
+wget https://raw.githubusercontent.com/vmaaik/aws-demo/master/scripts/connect.php
